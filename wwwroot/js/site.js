6081e64fa75ebf269b1bb62f14f2e8a1c9d55194
Matthew Cataldi <mcataloe@gmail.com>
2017-12-09 10:13:44 -0600

    Experimented with ActionLink vs. Url.Action

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -15,18 +15,48 @@ $(function(){
     // });
 
 
-    var drawingSurface = document.getElementById("myCanvas");
-    var context = drawingSurface.getContext("2d");
+    // var drawingSurface = document.getElementById("myCanvas");
+    // var context = drawingSurface.getContext("2d");
 
-    context.lineWidth = 5;
-    context.strokeStyle = '#0f0';
-    context.lineCap = "round";
+
+    // // Circle
+    // context.beginPath();
+    // context.moveTo(10, 10);
+    // context.lineTo(225, 350);
+    // context.lineTo(300, 10);
+    // context.lineTo(400, 350);
+    // context.stroke();
+
+    // // Smiley Face!
+    // context.beginPath();
+    // context.arc(100, 150, 75, 1 * Math.PI, 2 * Math.PI, true);
+    // context.lineWidth = 25;
+    // context.strokeStyle = 'red';
+    // context.lineCap = "round";
+    // context.stroke();
+    
+    // context.beginPath();
+    // context.arc(50, 100, 5, 0, 2 * Math.PI, true);
+    // context.lineWidth = 25;
+    // context.strokeStyle = 'red';
+    // context.lineCap = "round";
+    // context.stroke();
+
+    // context.beginPath();
+    // context.arc(150, 100, 5, 0, 2 * Math.PI, true);
+    // context.lineWidth = 25;
+    // context.strokeStyle = 'red';
+    // context.lineCap = "round";
+    // context.stroke();
+
+    var myCanvas = document.getElementById("myCanvas");
+    var context = myCanvas.getContext("2d");
 
     context.beginPath();
-    context.moveTo(10, 10);
-    context.lineTo(225, 350);
-    context.lineTo(300, 10);
-    context.lineTo(400, 350);
+    context.moveTo(10, 380);
+    context.quadraticCurveTo(300, -250, 580, 380);
+    context.lineWidth = 25;
+    context.strokeStyle = "orange";
+    context.lineCap = "round";
     context.stroke();
-
 });
